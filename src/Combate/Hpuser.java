343250caeb151f41e2438fe0b10eb6bf07b8c6b5
Cjchn <carlosjchamorro@gmail.com>
2023-11-02 10:52:25 +0100

    final final

--- src/Combate/Hpuser.java
+++ src/Combate/Hpuser.java
@@ -4,6 +4,7 @@ import static Combate.Configuracion.*;
 
 import java.util.Scanner;
 
+import java.util.Random;
 
 
 public class Hpuser {
@@ -29,7 +30,7 @@ public class Hpuser {
         for(i = 0; i < text.length(); i++){
             System.out.printf("%c", text.charAt(i));
         try{
-            Thread.sleep(25);
+            Thread.sleep(0);
         }catch(InterruptedException ex){
             Thread.currentThread().interrupt();
         }
@@ -51,7 +52,7 @@ public class Hpuser {
     
     public static void main(String args[]) throws InterruptedException{
 
-        
+        Random aleatorio = new Random();
 
         String calavera=ANSI_CYAN+"           ______\r\n" + //
                 "        .-\"      \"-.\r\n" + //
@@ -88,7 +89,7 @@ public class Hpuser {
         String eleccion=null;
         String eleccion2=null;
         boolean buc=false;
-        int vidaUser=100;
+        int vidaUser=0;
         String TXT_BIENVENIDA= "BIENVENIDO AL MINIJUEGO";
         String TXT_EXPLICACION= "EXPLICACIÓN" ;
         Scanner sc=new Scanner(System.in);
@@ -118,11 +119,11 @@ public class Hpuser {
                         buc=true;
                             switch(eleccion2){
                                 case "a","comer":
-                                    Estructura(ANSI_BLUE+"Al comer esa basura, sacias un poco tu hambre."+ANSI_GREEN+"\n\t\t\t(Recuperas un poco de salud)"+ANSI_RESET);
+                                    Estructura(ANSI_BLUE+"Al comer esa basura, sacias un poco tu hambre."+ANSI_RESET);
                                     vidaUser=vidaUser+5;
                                 break;
                                 case "b","perro":
-                                    Estructura(ANSI_BLUE+"El perro muy asustado te muerde. Provocándote una herida en el antebrazo."+ANSI_GREEN+"\n\t\t\t (Pierdes puntos salud)"+ANSI_BLUE+"\nTe ves obligado a estrangular al perro con su propia cadena hasta asfixiarlo"+ANSI_RESET);
+                                    Estructura(ANSI_BLUE+"El perro muy asustado te muerde. Provocándote una herida en el antebrazo."+ANSI_BLUE+"\nTe ves obligado a estrangular al perro con su propia cadena hasta asfixiarlo"+ANSI_RESET);
                                     vidaUser=vidaUser-15;
                                 break;
                                 case "c","nada":
@@ -143,12 +144,12 @@ public class Hpuser {
                         buc=true;
                             switch(eleccion2){
                                 case "a":
-                                    Estructura(ANSI_BLUE+"La anciana la bebe y se muere, era"+ANSI_RED+" veneno..."+ANSI_GREEN+"\n\t\t\t(Tu salud aumenta por la buena decision)"+ANSI_RESET);
+                                    Estructura(ANSI_BLUE+"La anciana la bebe y se muere, era"+ANSI_RED+" veneno..."+ANSI_RESET);
                                     vidaUser=vidaUser+
                                     10;
                                 break;
                                 case "b":
-                                    Estructura(ANSI_BLUE+"Al poco tiempo, empiezas a tener fiebre, vomitas sangre y se te nubla la visión"+ANSI_GREEN+"\n\t\t\t(Tu salud disminuye bastante)"+ANSI_RESET);
+                                    Estructura(ANSI_BLUE+"Al poco tiempo, empiezas a tener fiebre, vomitas sangre y se te nubla la visión"+ANSI_RESET);
                                     vidaUser=vidaUser-25;
                                 break;
                                 default:
@@ -176,15 +177,15 @@ public class Hpuser {
         eleccion=sc.nextLine().toLowerCase();
         switch(eleccion){
             case "1","4","6":
-                Estructura2(ANSI_GREEN+"\t\t\t(Tu salud sube ligeramente)"+ANSI_RESET);
+                Estructura2(ANSI_BLUE+"\t\t\t(Este objeto te es de valor.)"+ANSI_RESET);
                 vidaUser=vidaUser+5;
             break;
             case "2","3","5":
-                Estructura2(ANSI_GREEN+"\t\t\t(Tu salud baja ligeramente)"+ANSI_RESET);
+                Estructura2(ANSI_BLUE+"\t\t\t(Este objeto es tan inutil que hasta te molesta...)"+ANSI_RESET);
                 vidaUser=vidaUser-5;
             break;
             default:
-                Estructura2(ANSI_BLUE+"No has elegido nada, por espabilao...\n"+ANSI_GREEN);
+                Estructura2(ANSI_BLUE+"No has elegido nada, por espabilao...\n"+ANSI_RESET);
         }
         Imprimir(ANSI_BLUE+"Acto seguido, la puerta de la sala se abre ¿quieres salir?\n"+ANSI_RESET);
         System.out.println(ANSI_PURPLE+"\t(A)-Si, salir. \t(B)-No, no salir. \t(C)-Esperar."+ANSI_RESET);
@@ -195,7 +196,7 @@ public class Hpuser {
             Estructura(ANSI_BLUE+"Sales con normalidad"+ANSI_RESET);
             continuar(sc);
         }else{
-            Estructura(ANSI_BLUE+"Entran dos individuos totalmente tapados con una capa y un capirote blanco, te someten descargas eléctricas y te sacan a la fuerza de la habitación, seguidamente, ellos se quedan dentro de la sala y cierran la puerta\n"+ANSI_GREEN+"(Pierdes un poco de salud)"+ANSI_RESET);
+            Estructura(ANSI_BLUE+"Entran dos individuos totalmente tapados con una capa y un capirote blanco, te someten descargas eléctricas y te sacan a la fuerza de la habitación, seguidamente, ellos se quedan dentro de la sala y cierran la puerta\n"+ANSI_RESET);
             continuar(sc);
         }
          Estructura(ANSI_BLUE+"La misma voz que te dio las instrucciones, vuelve a aparecer:\n"+RED_BOLD+"<< Te encuentras en un pasillo, si haces las cosas bien podrás salir de aquí y volver con tus compañeros... Ves tres puertas ¿verdad?, abre la que consideres que es la correcta, tienes treinta segundos para elegir...>>"+ANSI_RESET+ANSI_BLUE+" \nTe acercas a observar cada una de las puertas:"+ANSI_RESET);
@@ -219,7 +220,7 @@ public class Hpuser {
             Estructura2(RED_BOLD+"<< Niños, llanto, cárcel, trauma, abuso >>\n << ¿Cual es la respuesta? >>"+ANSI_RESET);
             eleccion=sc.nextLine().toLowerCase();
             if(eleccion.equals("pederasta")||eleccion.equals("pedofilo")||eleccion.equals("pedófilo")||eleccion.equals("pedrasta")||eleccion.equals("pederastia")||eleccion.equals("pedrastia")){
-                Estructura(ANSI_BLUE+"El hombre es liberado, luego entran unos encapuchados y se lo llevan de allí, al hombre solo le da tiempo a decir 'gracias'. \n \t\t\t"+ANSI_GREEN+"(Pierdes un poco de salud)"+ANSI_RESET);
+                Estructura(ANSI_BLUE+"El hombre es liberado, luego entran unos encapuchados y se lo llevan de allí, al hombre solo le da tiempo a decir 'gracias'."+ANSI_RESET);
                 vidaUser=vida_user-5;
                 i=4;
             }else{
@@ -229,30 +230,116 @@ public class Hpuser {
         }while(i<3);
         
         if(i==3){
-            Estructura(ANSI_BLUE+"La trampilla de la horca se abre, y el sujeto cae suspendido al vacío, rompiéndose el cuello en el acto por la soga que tenía atada.\n\t\t\t"+ANSI_GREEN+"(Ganas un poco de salud)"+ANSI_RESET);
+            Estructura(ANSI_BLUE+"La trampilla de la horca se abre, y el sujeto cae suspendido al vacío, rompiéndose el cuello en el acto por la soga que tenía atada."+ANSI_RESET);
             vidaUser=vidaUser+5;
         }
         continuar(sc);
 
         Estructura(RED_BOLD+"<< Quedará en tu consciencia si has hecho lo correcto o no con el sujeto; Agustín, 57 años, párroco de la iglesia del Bendito Corazón de Dios, condenado por abuso sexual a menores en 7 casos diferentes. >> \n "+ANSI_RESET+ANSI_BLUE+"Sigues en la habitación, no has recibido ninguna instrucción más y estás demasiado asustado para gritarle a la voz para que te saque de ahí, decides intentar salir por ti mismo:"+ANSI_RESET);
-        System.out.println(ANSI_PURPLE+"A) Ir hacia la puerta por la que entraste, pero está bloqueada (no hay efecto)\n" + //
+        
+        do{
+            System.out.println(ANSI_PURPLE+"A) Ir hacia la puerta por la que entraste, pero está bloqueada.\n" + //
                 "\n" + //
-                "B) Ir hacia la puerta situada detrás de la horca, pero está bloqueada (no hay efecto)\n" + //
+                "B) Ir hacia la puerta situada detrás de la horca, pero está bloqueada.\n" + //
                 "\n" + //
-                "C) Ir hacia la estantería vacía que se encuentra al este de la sala e intentar moverla, pero pesa demasiado y no puedes (no hay efecto)\n" + //
+                "C) Ir hacia la estantería vacía que se encuentra al este de la sala e intentar moverla, pero pesa demasiado y no puedes.\n" + //
                 "\n" + //
                 "D) Te subes a la horca, y ves que abajo hay una trampilla de madera sin cerradura, la abres, y bajas por una escalera de pared por un túnel muy oscuro."+ANSI_RESET);
-        eleccion=sc.nextLine().toLowerCase();
-        switch (eleccion) {
+            eleccion=sc.nextLine().toLowerCase();
+            switch (eleccion) {
+                case "a","b","c":
+                    Estructura2(ANSI_BLUE+"Tristemente, no sirve para nada"+ANSI_RESET);
+                    buc=false;
+                break;
+                case "d":
+                    int j=0;
+                    while(j<1){
+                    Estructura(ANSI_BLUE+"Tras bajar la escalera, te encuentras en un túnel de piedra que se bifurca en dos caminos, el izquierdo, y el derecho."+ANSI_RESET+ANSI_PURPLE+"\n ¿cuál quieres tomar? \n\t(A) Izquierdo \t (B) Derecho");
+                    eleccion2=sc.nextLine().toLowerCase();
+                    if(eleccion2.equals("a")||eleccion2.equals("izquierdo")){
+                        Estructura(ANSI_BLUE+"Recorres el túnel buscando una salida. Desemboca en un portón de madera de gran tamaño con adornos metálicos, el portón está abierto y decides cruzarlo, llegas a una habitación, dentro de la habitación había un hombre sentado en una silla. El hombre tenía la cara pálida y los ojos rojos."+ANSI_RESET);
+                        vidaUser=vidaUser-5;
+                        j++;
+                        continuar(sc);
+                    }else if(eleccion2.equals("derecho")||eleccion2.equals("b")){
+                        Estructura(ANSI_BLUE+"Recorres el túnel buscando una salida. Desemboca en un portón de madera de gran tamaño con adornos metálicos, el portón está abierto y decides cruzarlo, llegas a una habitación, dentro de la habitación había un hombre sentado en una silla. El hombre tenía la cara pálida y los ojos rojos."+ANSI_RESET);
+                        vidaUser=vidaUser+5;
+                        j++;
+                        continuar(sc);
+                    }else{
+                        Estructura2(ANSI_RED+"\t\t\t Derecha... o Izquierda..."+ANSI_RESET);
+                    }
+                }
+                buc=true;
+                break;
+            default:
+                System.out.println(ANSI_RED+"\t\t\tIntentalo de nuevo..."+ANSI_RESET);
+                continuar(sc);
+            }
+        }while(!buc);
 
-        }
+        do{
+            Estructura(ANSI_PURPLE+"En tu cabeza solo ves 2 posibles decisiones... \n\t(A)-Te das media vuelta y empiezas a correr.\t(B)-Con total naturalidad entras y le preguntas que le pasa, ves que se encuentra atado.");
+            eleccion=sc.nextLine().toLowerCase();
+            switch (eleccion) {
+                case "a":
+                int r=aleatorio.nextInt(4);
+                    switch (r) {
+                        case 1:
+                            Estructura(ANSI_BLUE+"Recapacitas y decides que no es para tanto, que podrías ayudarle (el hombre de la habitación te captura y te tiene como su rehén quitándote"+ANSI_RESET);
+                            vidaUser=vidaUser-10;
+                            continuar(sc);
+                        break;
+                        case 2:
+                            Estructura(ANSI_BLUE+"Consigues encontrar unas escaleras pero mientras mas te acercas mas gritos de dolor escuchas (son amigos tuyos viendo una película de miedo a todo volumen, te das cuenta que todo ha sido una alucinación"+ANSI_RESET);
+                            vidaUser=vidaUser+10;
+                            continuar(sc);
+                        break;
+                        case 3:
+                            Estructura(ANSI_BLUE+"Te tropiezas con la alfombra y te deja inconsciente"+ANSI_RESET);
+                            continuar(sc);
+                        break;
+                    }
+                    buc=true;
+                break;
+                case "b":
+                int p=aleatorio.nextInt(4);
+                    switch (p) {
+                        case 1:
+                            Estructura(ANSI_BLUE+"Decides soltarlo...\n Te ayuda a encontrar la salida."+ANSI_RESET);
+                            vidaUser=vidaUser+10;
+                            continuar(sc);
+                        break;
+                        case 2:
+                            Estructura(ANSI_BLUE+"Le interrogas\nesta tan perturbado que no consigues que diga nada \n pero consigues encontrar lo que parece una salida."+ANSI_RESET);
+                            continuar(sc);
+                        break;
+                        case 3:
+                            Estructura(ANSI_BLUE+"Está en muy mal estado y solo dice cosas sin sentido. \n hace tanto ruido que despierta al verdadero asesino que tiene capturado al hombre");
+                            vidaUser=vidaUser-10;
+                            continuar(sc);
+                        break;
+                    }
+                    buc=true;
+                break;
+                default:
+                    Imprimir(ANSI_BLUE+"\nTienes tanto miedo que no sabes que hacer bien..."+ANSI_RESET);
+                    continuar(sc);
+                    buc=false;
+            }
+        }while(!buc);
 
-        System.out.println(ANSI_BLUE+"");
+        Estructura(ANSI_BLUE+"Finalmente consigues salir de esta pesadilla. \n Lo unico que recuerdas claramente es un numero... recuerdalo bien:"+ANSI_RESET+BLUE_BOLD+"\n\t\t\t"+vidaUser);
+        continuar(sc);
 
+        TXT_BIENVENIDA="¡GRACIAS POR JUGAR!";
+        TXT_EXPLICACION="FINALMENTE";
 
-
-
-
+        Separador();
+        System.out.println(ANSI_PURPLE + TXT_BIENVENIDA + "\n" + SUBRAYA.repeat(TXT_BIENVENIDA.length())+ ANSI_RESET);
+        System.out.println("\n"+ANSI_BLUE + TXT_EXPLICACION + "\n" + SUBRAYA.repeat(TXT_EXPLICACION.length()));
+        System.out.println(ANSI_BLUE+"Aqui acaba la Historia interactiva... \n  una vez mas Recuerda este numero es MUY importante: "+vidaUser+"\n ¡ADIOS!"+ANSI_RESET);
+        Separador();
 
     }
 }
